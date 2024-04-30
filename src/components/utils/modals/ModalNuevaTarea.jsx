f642f2ab521cb7bbc39ccf8fa0c34dd36400105c
Mauricio Apaza Carpio <mauricio.carpio@vfcons.com>
2024-04-30 12:29:41 -0500

    Correcion modales

--- src/components/utils/modals/ModalNuevaTarea.jsx
+++ src/components/utils/modals/ModalNuevaTarea.jsx
@@ -1,8 +1,6 @@
 'use client';
-import { useState, useMemo, useEffect } from "react";
+import { useState, useMemo, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-
-import { Select, SelectItem } from "@nextui-org/react";
 import {
   Modal,
   ModalContent,
@@ -17,27 +15,18 @@ import {
   DropdownMenu,
   DropdownItem,
   Checkbox,
-  select,
-} from "@nextui-org/react";
+} from '@nextui-org/react';
 import { crearTarea, obtenerTareasActividad } from '@/services/Prisma/Tarea';
 
-import AWS from "aws-sdk";
-
-import { FaSleigh } from "react-icons/fa";
+import AWS from 'aws-sdk';
 
 export default function ModalNuevaTarea({ idActi, idFluj, totalUsuarios, totalRol, setTareas }) {
-
   const router = useRouter();
-  const [showNoUsersModal, setShowNoUsersModal] = useState(false);
-  const [showNoRolModal, setshowNoRolModal] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const [selectedKeys, setSelectedKeys] = useState(new Set([]));
   const [selectedKeys2, setSelectedKeys2] = useState(new Set([]));
   const [selectedFiles, setSelectedFiles] = useState([]);
   const [tareasActividad, settareasActividad] = useState([]);
-
-  const [IdDependencia, setIdDependencia] = useState();
-
   const [isFileCheckboxSelected, setIsFileCheckboxSelected] = useState(false);
   const [isDependencyCheckboxSelected, setIsDependencyCheckboxSelected] = useState(false);
 
@@ -45,28 +34,29 @@ export default function ModalNuevaTarea({ idActi, idFluj, totalUsuarios, totalRo
     const getTareasActividad = async (idActi) => {
       const tareasActividad = await obtenerTareasActividad(idActi);
       settareasActividad(tareasActividad);
-    }
+    };
     getTareasActividad(idActi);
   }, [idActi]);
 
   const [values, setValues] = useState({
-    VCHNOMBRE: "",
-    VCHDESCRIPCION: "",
-    NUMDIASALERTA: "",
-    NUMDIASDURACION: "",
+    VCHNOMBRE: '',
+    VCHDESCRIPCION: '',
+    NUMDIASALERTA: '',
+    NUMDIASDURACION: '',
   });
   const [isSelected, setIsSelected] = useState(false);
   const [showWarning, setShowWarning] = useState(false);
 
-  const empresa = process.env.EMPRESA;
-  const isButtonDisabled = !idActi || idActi === "";
-
+  const empresa = process.env.CLIENTE;
+  const isButtonDisabled = !idActi || idActi === '';
 
   const handleSelectionChange = (keys) => {
+    console.log('usuarios ==>', keys);
     setSelectedKeys(keys);
     setValues({ ...values, USUARIOS: keys });
   };
   const handleSelectionChange2 = (keys) => {
+    console.log('roles ==>', keys);
     setSelectedKeys2(keys);
     setValues({ ...values, ROLES: keys });
   };
@@ -76,8 +66,16 @@ export default function ModalNuevaTarea({ idActi, idFluj, totalUsuarios, totalRo
   };
 
   const handleCreateTarea = async () => {
+    console.log('valores que se estan escribiendo', values);
     const { VCHNOMBRE, VCHDESCRIPCION, NUMDIASALERTA, NUMDIASDURACION } = values;
-    if (!VCHNOMBRE || !VCHDESCRIPCION || !NUMDIASALERTA || !NUMDIASDURACION || selectedKeys.size === 0 || selectedKeys2.size === 0) {
+    if (
+      !VCHNOMBRE ||
+      !VCHDESCRIPCION ||
+      !NUMDIASALERTA ||
+      !NUMDIASDURACION ||
+      selectedKeys.size === 0 ||
+      selectedKeys2.size === 0
+    ) {
       setShowWarning(true);
       return;
     }
@@ -86,6 +84,7 @@ export default function ModalNuevaTarea({ idActi, idFluj, totalUsuarios, totalRo
     await handleUploadToS3(folderName);
     const res = await crearTarea(values, idActi, idFluj, folderName);
 
+    console.log(res);
     if (res) {
       setTareas((prevTareas) => [...prevTareas, res]);
     }
@@ -93,26 +92,15 @@ export default function ModalNuevaTarea({ idActi, idFluj, totalUsuarios, totalRo
     handleOpenModal();
   };
 
-
   const handleOpenModal = () => {
-    if (totalUsuarios.length === 0) {
-      setShowNoUsersModal(true);
-      return;
-    }
-
-    if (totalRol.length === 0) {
-      setshowNoRolModal(true);
-      return;
-    }
-
     setSelectedKeys(new Set([]));
     setSelectedKeys2(new Set([]));
     setSelectedFiles([]);
     setValues({
-      VCHNOMBRE: "",
-      VCHDESCRIPCION: "",
-      NUMDIASALERTA: "",
-      NUMDIASDURACION: "",
+      VCHNOMBRE: '',
+      VCHDESCRIPCION: '',
+      NUMDIASALERTA: '',
+      NUMDIASDURACION: '',
     });
     setIsSelected(false);
     setIsDependencyCheckboxSelected(false);
@@ -120,11 +108,11 @@ export default function ModalNuevaTarea({ idActi, idFluj, totalUsuarios, totalRo
     setIsOpen(!isOpen);
   };
 
-
   const handleFileInputChange = (e) => {
     if (e.target.files && e.target.files.length > 0) {
       const files = e.target.files;
       setSelectedFiles(files);
+      console.log('Archivos seleccionados:', files);
 
       // Almacenar los nombres de los archivos en el estado values
       const fileNames = Array.from(files).map((file) => file.name);
@@ -133,9 +121,9 @@ export default function ModalNuevaTarea({ idActi, idFluj, totalUsuarios, totalRo
 
   const handleUploadToS3 = async (folderName) => {
     AWS.config.update({
-      region: "us-east-1",
-      accessKeyId: "AKIAVNIGRL6VQALTCM77",
-      secretAccessKey: "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",
+      region: 'us-east-1',
+      accessKeyId: 'AKIAVNIGRL6VQALTCM77',
+      secretAccessKey: '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',
     });
 
     const s3 = new AWS.S3();
@@ -152,15 +140,17 @@ export default function ModalNuevaTarea({ idActi, idFluj, totalUsuarios, totalRo
       const uniqueFileName = `${folderName}/${file.name}`;
 
       const params = {
-        Bucket: "expedientespjvf",
+        Bucket: 'expedientespjvf',
         Key: `archivos/${empresa}/${uniqueFileName}`,
         Body: file,
       };
 
       try {
         const data = await s3.upload(params).promise();
+        console.log('Archivo cargado exitosamente:', data);
         uploadedNames.push(uniqueFileName); // Agregar el nombre del archivo subido al array de nombres
       } catch (error) {
+        console.error('Error al cargar el archivo a S3:', error);
       }
     }
 
@@ -171,9 +161,9 @@ export default function ModalNuevaTarea({ idActi, idFluj, totalUsuarios, totalRo
   const selectedUsersValue = useMemo(() => {
     const selectedUsers = Array.from(selectedKeys).map((key) => {
       const user = totalUsuarios.find((usuario) => usuario.NUMUSUARIOID === parseInt(key));
-      return user ? user.VCHCORREO.split("@")[0] : "";
+      return user ? user.VCHCORREO.split('@')[0] : '';
     });
-    return selectedUsers.join(", ");
+    return selectedUsers.join(', ');
   }, [selectedKeys]);
 
   const selectedRolesValue = useMemo(() => {
@@ -181,7 +171,7 @@ export default function ModalNuevaTarea({ idActi, idFluj, totalUsuarios, totalRo
       const rol = totalRol.find((rol) => rol.NUMROLID === parseInt(key));
       return rol?.VCHNOMBRE;
     });
-    return selectedRoles.join(", ");
+    return selectedRoles.join(', ');
   }, [selectedKeys2]);
 
   selectedRolesValue;
@@ -190,172 +180,156 @@ export default function ModalNuevaTarea({ idActi, idFluj, totalUsuarios, totalRo
       {idActi && (
         <Button
           onPress={handleOpenModal}
-          color="danger"
-          style={{ marginLeft: "20px" }}
-          disabled={isButtonDisabled} >
+          color='danger'
+          style={{ marginLeft: '20px' }}
+          disabled={isButtonDisabled}
+        >
           Crear Tarea
         </Button>
       )}
 
-      <Modal isOpen={showNoUsersModal} onClose={() => setShowNoUsersModal(false)}>
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={(open) => setIsOpen(open)}
+        placement='top-center'
+        size='xl'
+      >
         <ModalContent>
-          <ModalHeader>No existen usuarios</ModalHeader>
-          <ModalBody>No hay usuarios disponibles para asignar a la tarea.
-            <Button
-              color="danger"
-              className='py-2 px-4 rounded-md'
-              size='md'
-              onPress={() => router.push(`/dashboard/usuarios`)}>
-              Crear un Usuario
-            </Button>
-          </ModalBody>
-          <ModalFooter>
-            <Button onPress={() => setShowNoUsersModal(false)}>Cerrar</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-
-      <Modal isOpen={showNoRolModal} onClose={() => setshowNoRolModal(false)}>
-        <ModalContent>
-          <ModalHeader>No existen Roles</ModalHeader>
-          <ModalBody>No hay Roles disponibles para asignar a la tarea.
-            <Button
-              color="danger"
-              className='py-2 px-4 rounded-md'
-              size='md'
-              onPress={() => router.push(`/dashboard/roles`)}>
-              Crear un Rol
-            </Button>
-          </ModalBody>
-          <ModalFooter>
-            <Button onPress={() => setshowNoRolModal(false)}>Cerrar</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-
-      <Modal isOpen={showWarning} onOpenChange={() => setShowWarning(false)}>
-        <ModalContent>
-          <ModalHeader>Advertencia</ModalHeader>
-          <ModalBody>Por favor, complete todos los campos obligatorios.
-          </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onPress={() => setShowWarning(false)}>
-              Entendido
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-
-
-      <Modal isOpen={isOpen} onOpenChange={(open) => setIsOpen(open)} placement="top-center">
-        <ModalContent>
-          <ModalHeader className="flex flex-col gap-1">Nueva Tarea</ModalHeader>
+          <ModalHeader className='flex flex-col gap-1'>Nueva Tarea</ModalHeader>
           <ModalBody>
-            <Input
-              autoFocus label="Ingrese un nombre*"
-              name="VCHNOMBRE"
-              variant="bordered"
-              isClearable onChange={handleValues}
-              defaultValue={values.VCHNOMBRE} />
-
-            <Input
-              autoFocus
-              label="Ingrese una descripcion *"
-              name="VCHDESCRIPCION"
-              variant="bordered"
-              isClearable
-              onChange={handleValues}
-              defaultValue={values.VCHDESCRIPCION}
-            />
-            <Input
-              type="number"
-              autoFocus
-              label="Dias de Duracion*"
-              name="NUMDIASDURACION"
-              variant="bordered"
-              isClearable
-              onChange={handleValues}
-              defaultValue={values.VCHDIASDURACION}
-            />
-            <Input
-              type="number"
-              autoFocus
-              label="Dias de Alerta*"
-              name="NUMDIASALERTA"
-              variant="bordered"
-              isClearable
-              onChange={handleValues}
-              defaultValue={values.NUMDIASALERTA}
-            />
-            <div>Seleccione los usuarios asignados</div>
-            <Dropdown>
-              <DropdownTrigger>
-                <Button variant="bordered" className="uppercase">
-                  {selectedUsersValue === "" ? "Seleccione Usuarios*" : selectedUsersValue}
+            {totalUsuarios.length === 0 && (
+              <div className='flex flex-col justify-center items-center gap-4 w-full'>
+                <p>No hay usuarios disponibles para seleccionar.</p>
+                <Button color='primary' onPress={() => router.push('/dashboard/usuarios')}>
+                  Agregar Usuarios
                 </Button>
-              </DropdownTrigger>
-              <DropdownMenu
-                aria-label="Seleccione usuarios*"
-                variant="flat"
-                closeOnSelect={false}
-                disallowEmptySelection={false}
-                selectionMode="multiple"
-                selectedKeys={selectedKeys}
-                onSelectionChange={handleSelectionChange}
-              >
-                {totalUsuarios.map((usuario) => (
-                  <DropdownItem key={usuario.NUMUSUARIOID} value={usuario.VCHCORREO}>
-                    {usuario.VCHCORREO}
-                  </DropdownItem>
-                ))}
-              </DropdownMenu>
-            </Dropdown>
-            <div>Seleccione los Roles</div>
-            <Dropdown>
-              <DropdownTrigger>
-                <Button variant="bordered" className="uppercase">
-                  {selectedRolesValue === "" ? "Seleccione Roles*" : selectedRolesValue}
+              </div>
+            )}
+            {totalRol.length === 0 && (
+              <div className='flex flex-col justify-center items-center gap-4 w-full'>
+                <p>No hay roles disponibles para seleccionar.</p>
+                <Button color='primary' onPress={() => router.push('/dashboard/roles')}>
+                  Agregar Roles
                 </Button>
-              </DropdownTrigger>
-              <DropdownMenu
-                aria-label="Seleccione Roles*"
-                variant="flat"
-                closeOnSelect={false}
-                disallowEmptySelection={false}
-                selectionMode="multiple"
-                selectedKeys={selectedKeys2}
-                onSelectionChange={handleSelectionChange2}
-              >
-                {totalRol?.map((usuario) => (
-                  <DropdownItem key={usuario.NUMROLID} value={usuario.VCHNOMBRE}>
-                    {usuario.VCHNOMBRE}
-                  </DropdownItem>
-                ))}
-              </DropdownMenu>
-            </Dropdown>
+              </div>
+            )}
+            {showWarning && (
+              <p className='text-red-500'>Por favor, rellene todos los campos obligatorios. (*)</p>
+            )}
+            {totalUsuarios.length > 0 && totalRol.length > 0 && (
+              <>
+                <Input
+                  autoFocus
+                  label='Ingrese un nombre (*)'
+                  name='VCHNOMBRE'
+                  variant='bordered'
+                  isClearable
+                  onChange={handleValues}
+                  defaultValue={values.VCHNOMBRE}
+                />
 
-            <Checkbox
-              isSelected={isFileCheckboxSelected}
-              onValueChange={setIsFileCheckboxSelected}
-            >
-              ¿Va a cargar archivos?
-            </Checkbox>
-            {isFileCheckboxSelected && (
-              <input
-                type="file"
-                name="ARCHIVOS"
-                onChange={handleFileInputChange}
-                multiple // Permite la selección de múltiples archivos
-              />
+                <Input
+                  autoFocus
+                  label='Ingrese una descripcion (*)'
+                  name='VCHDESCRIPCION'
+                  variant='bordered'
+                  isClearable
+                  onChange={handleValues}
+                  defaultValue={values.VCHDESCRIPCION}
+                />
+                <Input
+                  type='number'
+                  autoFocus
+                  label='Dias de Duracion (*)'
+                  name='NUMDIASDURACION'
+                  variant='bordered'
+                  isClearable
+                  onChange={handleValues}
+                  defaultValue={values.VCHDIASDURACION}
+                />
+                <Input
+                  type='number'
+                  autoFocus
+                  label='Dias de Alerta (*)'
+                  name='NUMDIASALERTA'
+                  variant='bordered'
+                  isClearable
+                  onChange={handleValues}
+                  defaultValue={values.NUMDIASALERTA}
+                />
+                <div>Seleccione los usuarios asignados</div>
+                <Dropdown>
+                  <DropdownTrigger>
+                    <Button variant='bordered' className='uppercase'>
+                      {selectedUsersValue === '' ? 'Seleccione Usuarios (*)' : selectedUsersValue}
+                    </Button>
+                  </DropdownTrigger>
+                  <DropdownMenu
+                    aria-label='Seleccione usuarios*'
+                    variant='flat'
+                    closeOnSelect={false}
+                    disallowEmptySelection={false}
+                    selectionMode='multiple'
+                    selectedKeys={selectedKeys}
+                    onSelectionChange={handleSelectionChange}
+                  >
+                    {totalUsuarios.map((usuario) => (
+                      <DropdownItem key={usuario.NUMUSUARIOID} value={usuario.VCHCORREO}>
+                        {usuario.VCHCORREO}
+                      </DropdownItem>
+                    ))}
+                  </DropdownMenu>
+                </Dropdown>
+                <div>Seleccione los Roles</div>
+                <Dropdown>
+                  <DropdownTrigger>
+                    <Button variant='bordered' className='uppercase'>
+                      {selectedRolesValue === '' ? 'Seleccione Roles (*)' : selectedRolesValue}
+                    </Button>
+                  </DropdownTrigger>
+                  <DropdownMenu
+                    aria-label='Seleccione Roles*'
+                    variant='flat'
+                    closeOnSelect={false}
+                    disallowEmptySelection={false}
+                    selectionMode='multiple'
+                    selectedKeys={selectedKeys2}
+                    onSelectionChange={handleSelectionChange2}
+                  >
+                    {totalRol?.map((usuario) => (
+                      <DropdownItem key={usuario.NUMROLID} value={usuario.VCHNOMBRE}>
+                        {usuario.VCHNOMBRE}
+                      </DropdownItem>
+                    ))}
+                  </DropdownMenu>
+                </Dropdown>
+
+                <Checkbox
+                  isSelected={isFileCheckboxSelected}
+                  onValueChange={setIsFileCheckboxSelected}
+                >
+                  ¿Va a cargar archivos?
+                </Checkbox>
+                {isFileCheckboxSelected && (
+                  <input
+                    type='file'
+                    name='ARCHIVOS'
+                    onChange={handleFileInputChange}
+                    multiple // Permite la selección de múltiples archivos
+                  />
+                )}
+              </>
             )}
           </ModalBody>
           <ModalFooter>
-            <Button color="danger" variant="flat" onPress={() => setIsOpen(false)}>
+            <Button color='danger' variant='flat' onPress={() => setIsOpen(false)}>
               Cerrar
             </Button>
-            <Button color="primary" onPress={handleCreateTarea}>
-              Crear Tarea
-            </Button>
+            {totalUsuarios.length > 0 && totalRol.length > 0 && (
+              <Button color='primary' onPress={handleCreateTarea}>
+                Crear Tarea
+              </Button>
+            )}
           </ModalFooter>
         </ModalContent>
       </Modal>
